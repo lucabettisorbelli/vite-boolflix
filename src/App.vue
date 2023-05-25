0070566965b7e706a0ef458d59657fce6016b835
luca <Luca betti sorbelli>
2023-05-25 16:45:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppHeader from "./components/AppHeader.vue"
+import AppMain from "./components/AppMain.vue"
+
+import { store } from "./data/store.js"
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppMain
+  },
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
+
+<style scoped></style>
