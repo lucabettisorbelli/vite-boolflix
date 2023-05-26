7dcab69d3dabf868c22d150a2a3ec86a7f5745a5
luca <Luca betti sorbelli>
2023-05-26 09:22:31 +0200

    milenstone 1

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,79 @@
 <script>
+import { store } from "../data/store.js"
+import axios from 'axios'
+
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            store,
+            query: "",
+            movies: []
+        }
+    },
+    methods: {
+        searchMovies() {
+            const url = `${store.url}&query=${this.query}`;
+
+            axios.get(url)
+                .then(risposta => {
+                    this.movies = risposta.data.results;
+                });
+        }
+    }
+}
 </script>
 
-<template></template>
+<template>
+    <header>
+        <div class="container">
+            <div class="titolo">
+                <h1>BoolFlix</h1>
+            </div>
+            <div class="boxSearch">
+                <div class="input">
+                    <input type="text" v-model="query" @keyup="searchMovies" placeholder="Cerca film">
+                </div>
+                <div class="button">
+                    <button @click="searchMovies">Cerca</button>
+                </div>
+            </div>
+            <div class="boxFilm">
+                <div class="imgArea">
+                    <div class="box">
+                        <ul>
+                            <li v-for="movie in movies">
+                                <h2>{{ movie.title }}</h2>
+                                <p>Titolo originale: {{ movie.original_title }}</p>
+                                <p>Lingua: {{ movie.original_language }}</p>
+                                <p>Voto: {{ movie.vote_average }}</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
+}
+
+.titolo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50px;
+}
+
+.boxSearch {
+    display: flex;
+
+    .input {
+        padding-right: 20px;
+    }
+}
+</style>
--- src/data/store.js
+++ src/data/store.js
@@ -1,5 +1,13 @@
 import { reactive } from "vue";
 
+const apiKey = "1c2a1c94a669f2c00dc661ebc0a13765";
+const query = "";
+const language = "";
+const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}`;
+
 export const store = reactive({
-  loading: true,
+  apiKey,
+  query,
+  language,
+  url,
 });
