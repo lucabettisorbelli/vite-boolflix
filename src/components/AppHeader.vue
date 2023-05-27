02a5886f52948f11f8350822f4cc581fcf78433c
luca <Luca betti sorbelli>
2023-05-27 16:29:30 +0200

    stelle + correzione per film/serie

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,39 +1,56 @@
 <script>
 import { store } from "../data/store.js";
 import axios from "axios";
+import '@fortawesome/fontawesome-free/css/all.css';
 
 export default {
     name: "AppHeader",
     data() {
         return {
             store,
-            query: "",
+            queryFilm: "",
+            querySeries: "",
         };
+    },
+    computed: {
+        searchBoth() {
+            if (this.queryFilm === "" && this.querySeries === "") {
+                return store.movies;
+            } else if (this.queryFilm !== "") {
+                return store.movies;
+            } else {
+                return store.series;
+            }
+        },
     },
     methods: {
         searchMovies() {
-            if (this.query === "") {
+            if (this.queryFilm === "") {
                 this.getTopMovies();
             } else {
-                const url = `${store.url}api_key=${store.apiKey}&query=${this.query}`;
+                const url = `${store.url}api_key=${store.apiKey}&query=${this.queryFilm}`;
                 axios.get(url).then((risposta) => {
                     this.store.movies = risposta.data.results;
-                    console.log(risposta.data);
+                    console.log('film', this.store.movies);
+
                 });
             }
         },
         searchSeries() {
-            const url = `https://api.themoviedb.org/3/search/tv?api_key=${store.apiKey}&query=${this.query}`;
-            axios.get(url).then((risposta) => {
-                this.store.movies = risposta.data.results;
-                console.log(risposta.data);
-            });
+            if (this.querySeries === "") {
+                this.getTopMovies();
+            } else {
+                const url = `https://api.themoviedb.org/3/search/tv?api_key=${store.apiKey}&query=${this.querySeries}`;
+                axios.get(url).then((risposta) => {
+                    this.store.series = risposta.data.results;
+                    console.log('serie', this.store.series);
+                });
+            }
         },
         getTopMovies() {
             const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${store.apiKey}`;
             axios.get(url).then((risposta) => {
                 this.store.movies = risposta.data.results;
-                console.log(risposta.data);
             });
         }
     },
@@ -52,15 +69,17 @@ export default {
             </div>
             <div class="boxSearch">
                 <div class="input">
-                    <input type="text" v-model="query" @keyup="searchMovies" placeholder="Cerca film" />
+                    <input type="text" v-model="queryFilm" @keyup="searchMovies" placeholder="Cerca film " />
+                    <button @click="searchMovies">Cerca film</button>
                 </div>
                 <div class="button">
-                    <button @click="searchMovies">Cerca</button>
+                    <input type="text" v-model="querySeries" @keyup="searchSeries" placeholder="Cerca serie tv" />
+                    <button @click="searchSeries">Cerca Serie tv</button>
                 </div>
             </div>
             <div class="boxFilm">
                 <div class="cardsArea">
-                    <div v-for="movie in store.movies" class="card">
+                    <div v-for="movie in searchBoth" class="card">
                         <div class="cardImg">
                             <img v-if="movie.poster_path == null"
                                 src="https://th.bing.com/th/id/R.ae0fc67b38f2d8fee7b3976df6ec8acc?rik=7wdWtRhFfNGMhQ&pid=ImgRaw&r=0" />
@@ -74,6 +93,10 @@ export default {
                                     :src="`https://unpkg.com/language-icons/icons/${movie.original_language}.svg`">
                             </div>
                             <p>Voto: {{ movie.vote_average }}</p>
+                            <div class="stars">
+                                <i v-for="n in Math.ceil(movie.vote_average / 2)" class="fas fa-star"></i>
+                                <i v-for="n in 5 - Math.ceil(movie.vote_average / 2)" class="far fa-star"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -141,5 +164,6 @@ export default {
             }
         }
     }
+
 }
 </style>
--- src/data/store.js
+++ src/data/store.js
@@ -4,6 +4,7 @@ export const store = reactive({
   apiKey: "1c2a1c94a669f2c00dc661ebc0a13765",
   language: "",
   movies: [],
+  series: [],
   url: `https://api.themoviedb.org/3/search/movie?`,
   imgPath: "https://image.tmdb.org/t/p/w500",
 });
